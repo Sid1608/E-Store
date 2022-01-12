5d619698a705f096336279d0db06936116948b3f
Sid1608 <akarsiddharth@gmail.com>
2022-01-12 15:59:46 +0530

    Shopping category page completed

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './App.css';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <ProductList/>
   )
 }
 
