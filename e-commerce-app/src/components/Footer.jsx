36e3cce5528c462d997ee1dac39b3d059e406a2f
Sid1608 <akarsiddharth@gmail.com>
2022-01-12 15:32:35 +0530

    Footer done

---
+++ e-commerce-app/src/components/Footer.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import styled from 'styled-components';
+import {
+    Facebook,
+    Instagram,
+    MailOutline,
+    Phone,
+    Pinterest,
+    Room,
+    Twitter,
+  } from "@mui/icons-material";
+const Container=styled.div `
+   display: flex;
+`;
+const Left=styled.div `
+   flex: 1;
+   display: flex;
+   flex-direction: column;
+   padding:20px;
+`;
+const Logo=styled.h1 ``
+const Desc=styled.p`
+    margin:20px 0px;
+`
+const SocialContainer=styled.div `
+    display: flex;
+
+`
+const SocialIcon=styled.div `
+    width:40px;
+    height:40px;
+    border-radius:50%;
+    color:white;
+    background-color:#${props=>props.color}
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    margin-right:20px;
+`
+const Center=styled.div `
+   flex:1;
+   padding:20px;
+`;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+//when you create ul it has it's own margin and padding by default delete then by below.
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Right=styled.div `
+   flex:1;
+   padding:20px;
+`;
+const ContactItem=styled.div `
+    margin-bottom:10px;
+    display:flex;
+    align-items:center;
+`;
+const Payment=styled.img `
+    width:50%;
+`;
+const Footer = () => {
+    return (
+        <Container>
+                   
+            <Left>
+                <Logo>Let's Shop</Logo>
+                <Desc>
+                    There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{marginRight:"10px"}}/>  3187 akar bhawan, kalyanji ka rasta chandpole bazar jaipur
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{marginRight:"10px"}}/>  +91 8824885175
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline style={{marginRight:"10px"}} /> akarsiddharth@gmail.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
