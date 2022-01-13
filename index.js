bfe988c2683b3eb170e6e2eea81ad0859d3cf155
Sid1608 <akarsiddharth@gmail.com>
2022-01-13 18:05:03 +0530

    mongodb connected to backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv=require("dotenv");
+const userRoute=require("./router/user")
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+)
+.then(()=>console.log("mongodb connected"))
+.catch(err=>console.log(err))
+
+
+app.listen(process.env.PORT||8080,()=>{
+    console.log(`server running on port 8080`)
+})
