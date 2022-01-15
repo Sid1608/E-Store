76c1b626e31120f3175905d7e0d1161b0a3e1abf
Sid1608 <akarsiddharth@gmail.com>
2022-01-15 09:36:14 +0530

    product route completed

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const mongoose = require("mongoose");
 const dotenv=require("dotenv");
 const userRoute=require("./router/user")
 const authRoute = require("./router/auth")
+const productRoute=require("./router/product")
 dotenv.config();
 
 mongoose.connect(
     process.env.MONGO_URL
 )
-.then(()=>console.log("mongodb connected"))
-.catch(err=>console.log(err));
+    .then(()=>console.log("mongodb connected"))
+    .catch(err=>console.log(err));
 
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+
 app.listen(process.env.PORT||8080,()=>{
     console.log(`server running on port 8080`)
 })
